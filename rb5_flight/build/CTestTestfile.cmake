9805c626f699622a7da8c6cef57c2e06c7d0bad7
angelcervant1 <angelcervant@hotmail.com>
2024-07-24 17:45:45 +0000

    make ws

---
+++ rb5_flight/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /ros_ws/rb5-flight-summer-dev_ws/rb5_flight/src
+# Build directory: /ros_ws/rb5-flight-summer-dev_ws/rb5_flight/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ rb5_flight/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/ros_ws/rb5-flight-summer-dev_ws/rb5_flight/devel:$CMAKE_PREFIX_PATH"
+export PWD='/ros_ws/rb5-flight-summer-dev_ws/rb5_flight/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/ros_ws/rb5-flight-summer-dev_ws/rb5_flight/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/ros_ws/rb5-flight-summer-dev_ws/rb5_flight/src:$ROS_PACKAGE_PATH"
