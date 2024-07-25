19df62cc3b96eeb14a8b3a9e8d2adcdbfa408d2e
angelcervant1 <angelcervant@hotmail.com>
2024-07-25 21:34:51 +0000

    add optitrack bridge to ros (no testing yet)

--- rb5_flight/build/CTestTestfile.cmake
+++ rb5_flight/build/CTestTestfile.cmake
@@ -5,4 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("optitrack-vrpn")
+subdirs("mocap_optitrack")
 subdirs("rb5_simulation")
