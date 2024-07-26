daaea05465185eb81fa739d11e35fe3d1fb58579
angelcervant1 <angelcervant@hotmail.com>
2024-07-26 19:05:40 +0000

    NatNet client/server running

--- rb5_flight/src/rb5_simulation/scripts/drone_optitrack_rviz.py
+++ rb5_flight/src/rb5_simulation/scripts/drone_optitrack_rviz.py
@@ -1,51 +1,43 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 from tf.broadcaster import TransformBroadcaster
-import tf_conversions
 
-class DroneController:
+class DroneOptitrackFollower:
     def __init__(self):
         rospy.init_node('drone_teleop')
         self.pub = rospy.Publisher('/odom', Odometry, queue_size=10)
-        self.sub = rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
+        self.sub = rospy.Subscriber('/natnet_ros/RB5_Drone/pose', PoseStamped, self.pose_callback)
         self.odom_broadcaster = TransformBroadcaster()
         
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
-        self.th = 0.0
-        
-        self.vx = 0.0
-        self.vy = 0.0
-        self.vz = 0.0
-        self.vth = 0.0
+        self.wx = 0.0
+        self.wy = 0.0
+        self.wz = 0.0
+        self.w = 0.0
         
         self.current_time = rospy.Time.now()
         self.last_time = rospy.Time.now()
 
-    def cmd_vel_callback(self, msg):
-        self.vx = msg.linear.x
-        self.vy = msg.linear.y
-        self.vz = msg.linear.z
-        self.vth = msg.angular.z
-
+    def pose_callback(self, msg):
+        self.x = msg.pose.position.x
+        self.y = msg.pose.position.y
+        self.z = msg.pose.position.z
+        self.wx = msg.pose.orientation.x
+        self.wy = msg.pose.orientation.y
+        self.wz = msg.pose.orientation.z
+        self.w = msg.pose.orientation.w
 
     def update(self):
         self.current_time = rospy.Time.now()
-        dt = (self.current_time - self.last_time).to_sec()
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-        self.z += self.vz * dt
-        self.th += self.vth * dt
-
-        odom_quat = tf_conversions.transformations.quaternion_from_euler(0, 0, self.th)
 
         self.odom_broadcaster.sendTransform(
             (self.x, self.y, self.z),
-            odom_quat,
+            (self.wx, self.wy, self.wz, self.w),
             self.current_time,
             "base_link",
             "odom"
@@ -57,15 +49,11 @@ class DroneController:
         odom.pose.pose.position.x = self.x
         odom.pose.pose.position.y = self.y
         odom.pose.pose.position.z = self.z
-        odom.pose.pose.orientation.x = odom_quat[0]
-        odom.pose.pose.orientation.y = odom_quat[1]
-        odom.pose.pose.orientation.z = odom_quat[2]
-        odom.pose.pose.orientation.w = odom_quat[3]
+        odom.pose.pose.orientation.x = self.wx
+        odom.pose.pose.orientation.y = self.wy
+        odom.pose.pose.orientation.z = self.wz
+        odom.pose.pose.orientation.w = self.w
         odom.child_frame_id = "base_link"
-        odom.twist.twist.linear.x = self.vx
-        odom.twist.twist.linear.y = self.vy
-        odom.twist.twist.linear.z = self.vz
-        odom.twist.twist.angular.z = self.vth
 
         self.pub.publish(odom)
         self.last_time = self.current_time
@@ -77,7 +65,7 @@ class DroneController:
             rate.sleep()
 
 if __name__ == "__main__":
-    controller = DroneController()
+    controller = DroneOptitrackFollower()
     try:
         controller.run()
     except rospy.ROSInterruptException:
