cef0fc4a19261fc8e39459e52579f1784c5cf2ec
angelcervant1 <angelcervant@hotmail.com>
2024-07-29 17:55:30 +0000

    change twist msg

--- rb5_flight/src/rb5_navigation/src/teleop3d.cpp
+++ rb5_flight/src/rb5_navigation/src/teleop3d.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <iostream>
 
 class TeleopDrone
@@ -7,7 +7,7 @@ class TeleopDrone
 public:
     TeleopDrone()
     {
-        cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("mavros/setpoint_velocity/cmd_vel", 10);
+        cmd_vel_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("mavros/setpoint_velocity/cmd_vel", 10);
         startControlLoop();
     }
 
@@ -18,7 +18,7 @@ private:
     void startControlLoop()
     {
         char key;
-        geometry_msgs::Twist cmd_vel;
+        geometry_msgs::TwistStamped cmd_vel;
 
         while (ros::ok())
         {
@@ -27,17 +27,18 @@ private:
 
             switch (key)
             {
-                case 'w': cmd_vel.linear.x = 1.0; break;   // Move forward
-                case 's': cmd_vel.linear.x = -1.0; break;  // Move backward
-                case 'a': cmd_vel.linear.y = 1.0; break;   // Move left
-                case 'd': cmd_vel.linear.y = -1.0; break;  // Move right
-                case 'r': cmd_vel.angular.z = 1.0; break;   // Rotate clockwise
-                case 'f': cmd_vel.angular.z = -1.0; break;  // Rotate counter-clockwise
-                case 'u': cmd_vel.linear.z = 1.0; break;   // Move up
-                case 'j': cmd_vel.linear.z = -1.0; break;  // Move down
+                case 'w': cmd_vel.twist.linear.x = 1.0; break;   // Move forward
+                case 's': cmd_vel.twist.linear.x = -1.0; break;  // Move backward
+                case 'a': cmd_vel.twist.linear.y = 1.0; break;   // Move left
+                case 'd': cmd_vel.twist.linear.y = -1.0; break;  // Move right
+                case 'r': cmd_vel.twist.angular.z = 1.0; break;   // Rotate clockwise
+                case 'f': cmd_vel.twist.angular.z = -1.0; break;  // Rotate counter-clockwise
+                case 'u': cmd_vel.twist.linear.z = 1.0; break;   // Move up
+                case 'j': cmd_vel.twist.linear.z = -1.0; break;  // Move down
                 default: std::cout << "Invalid key. Use 'w', 's', 'a', 'd', 'r', 'f', 'u', or 'j'." << std::endl;
             }
-
+            cmd_vel.header.stamp = ros::Time::now();
+            cmd_vel.header.frame_id = "odom";
             cmd_vel_pub_.publish(cmd_vel);
             ros::spinOnce();
         }
